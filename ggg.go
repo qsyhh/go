179ba2a1f5f95c1094fec5ea3650d2a49861a28d
其实雨很好 <2122840028@qq.com>
2024-09-08 12:39:45 +0800

    Update ggg.go

--- ggg.go
+++ ggg.go
@@ -1,28 +1,48 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"net/http/httputil"
-	"net/url"
+    "flag"
+    "log"
+    "net/http"
+    "net/http/httputil"
+    "net/url"
 )
 
-func main() {
-	targetURL, err := url.Parse("http://enka.network")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	proxy := httputil.NewSingleHostReverseProxy(targetURL)
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		proxy.ServeHTTP(w, r)
-	})
-
-	log.Println("Starting proxy server on 0.0.0.0:7860")
-	err = http.ListenAndServe("0.0.0.0:7860", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
+type proxyHandler struct {
+    target string
 }
 
+func (h *proxyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    u, err := url.Parse(h.target)
+    if err != nil {
+        log.Fatalf("解析目标URL失败： %v", err)
+    }
+    if u.Scheme == "" {
+        log.Fatalf("目标URL的协议方案为空")
+    }
+    if u.Host == "" {
+        log.Fatalf("目标URL的主机名为空")
+    }
 
+    proxy := httputil.NewSingleHostReverseProxy(u)
+    proxy.Transport = &http.Transport{
+        DisableKeepAlives: true,
+    }
+
+    r.URL.Path = "/" // 设置路径为根路径
+    r.URL.RawQuery = "" // 移除查询参数
+    proxy.ServeHTTP(w, r)
+}
+
+func main() {
+    target := flag.String("target", "https://enka.network", "Target server to proxy")
+    listen := flag.String("listen", "0.0.0.0:7860", "Address to listen on")
+    flag.Parse()
+
+    if *target == "" || *listen == "" {
+        log.Fatal("请提供 -target 和 -listen 参数")
+    }
+
+    log.Printf("启动反向代理，监听 %s，转发到 %s", *listen, *target)
+    log.Fatal(http.ListenAndServe(*listen, &proxyHandler{target: *target}))
+}
