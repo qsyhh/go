41f4f18a8162b52e3fb8e49407ed43e9fbe8619e
其实雨很好 <2122840028@qq.com>
2024-09-08 12:47:49 +0800

    Update gggg.go

--- gggg.go
+++ gggg.go
@@ -8,28 +8,42 @@ import (
     "net/url"
 )
 
+type proxyHandler struct {
+    target string
+}
+
+func (h *proxyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    u, err := url.Parse(h.target)
+    if err != nil {
+        log.Fatalf("解析目标URL失败： %v", err)
+    }
+    if u.Scheme == "" {
+        log.Printf("目标URL的协议方案为空")
+        u.Scheme == "https://"
+    }
+    if u.Host == "" {
+        log.Fatalf("目标URL的主机名为空")
+    }
+    log.Printf("解析目标URL：", u)
+    proxy := httputil.NewSingleHostReverseProxy(u)
+    proxy.Transport = &http.Transport{
+        DisableKeepAlives: true,
+    }
+
+    r.URL.Path = "/" // 设置路径为根路径
+    r.URL.RawQuery = "" // 移除查询参数
+    proxy.ServeHTTP(w, r)
+}
+
 func main() {
-    target := flag.String("target", "https://enka.network", "Target server to proxy")
-    listen := flag.String("listen", "0.0.0.0:7860", "Address to listen on")
-    scheme := flag.String("scheme", "https", "Protocol scheme for the target URL")
+    target := flag.String("target", "", "Target server to proxy")
+    listen := flag.String("listen", "", "Address to listen on")
     flag.Parse()
 
     if *target == "" || *listen == "" {
         log.Fatal("请提供 -target 和 -listen 参数")
     }
 
-    u, err := url.Parse(*target)
-    if err != nil {
-        log.Fatalf("解析目标URL失败： %v", err)
-    }
-    u.Host = "enka.network"
-    u.Scheme = *scheme
-    if u.Scheme == "" {
-        log.Fatalf("目标URL的协议方案为空")
-    }
-
-    proxy := httputil.NewSingleHostReverseProxy(u)
     log.Printf("启动反向代理，监听 %s，转发到 %s", *listen, *target)
-    log.Fatal(http.ListenAndServe(*listen, proxy))
+    log.Fatal(http.ListenAndServe(*listen, &proxyHandler{target: *target}))
 }
-
