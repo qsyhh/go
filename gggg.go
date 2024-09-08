d3dced718ec8ebde530053c651a3812938eacad1
其实雨很好 <2122840028@qq.com>
2024-09-08 11:09:24 +0800

    Update gggg.go

--- gggg.go
+++ gggg.go
@@ -1,28 +1,30 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"net/http/httputil"
-	"net/url"
-	"os"
+    "flag"
+    "log"
+    "net/http"
+    "net/http/httputil"
+    "net/url"
 )
 
 func main() {
-	target := os.Getenv("TARGET")
-	listen := os.Getenv("LISTEN")
+    target := flag.String("target", "", "Target server to proxy")
+    listen := flag.String("listen", "", "Address to listen on")
+    flag.Parse()
 
-	if target == "" || listen == "" {
-		log.Fatal("请设置环境变量 TARGET 和 LISTEN")
-	}
+    if *target == "" || *listen == "" {
+        log.Fatal("请提供 -target 和 -listen 参数")
+    }
 
-	u, err := url.Parse(target)
-	if err != nil {
-		log.Fatalf("解析目标URL失败： %v", err)
-	}
+    u, err := url.Parse(*target)
+    if err != nil {
+        log.Fatalf("解析目标URL失败： %v", err)
+    }
 
-	proxy := httputil.NewSingleHostReverseProxy(u)
+    proxy := httputil.NewSingleHostReverseProxy(u)
 
-	log.Printf("启动反向代理，监听 %s，转发到 %s", listen, target)
-	log.Fatal(http.ListenAndServe(listen, proxy))
+    log.Printf("启动反向代理，监听 %s，转发到 %s", *listen, *target)
+    log.Fatal(http.ListenAndServe(*listen, proxy))
 }
+
